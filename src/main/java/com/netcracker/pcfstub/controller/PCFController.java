9e8d4a7b37fcf7b5a2bfece7001d02aa7640bf05
kabaye <svkulich@gmail.com>
2021-08-11 20:22:56 +0300

    feat: create PCF stub

--- src/main/java/com/netcracker/pcfstub/controller/PCFController.java
+++ src/main/java/com/netcracker/pcfstub/controller/PCFController.java
@@ -3,6 +3,7 @@ package com.netcracker.pcfstub.controller;
 import com.netcracker.pcfstub.model.SNA;
 import com.netcracker.pcfstub.model.SNR;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class PCFController {
     public SNA notify(SNR snr) {
         log.info("Received snr {}", snr);
         return new SNA().setSessionId(snr.getSupi());
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "TEST!!!";
     }
 }
