003d000d4dad4baa0482f53c6218041c57704341
kabaye <svkulich@gmail.com>
2021-08-11 19:49:04 +0300

    feat: init

---
+++ src/main/java/com/netcracker/pcfstub/PcfStubApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.pcfstub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfStubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcfStubApplication.class, args);
+    }
+
+}
